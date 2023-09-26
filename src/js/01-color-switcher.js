fa5895d78351fefb323fc79e72987623ea2922ac
Roman <roman.soldatov13@gmail.com>
2023-09-26 16:08:04 +0300

    try 1
    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let nIntervId;
 
+buttonStart.addEventListener('click', onStartclick);
+buttonStop.addEventListener('click', onStopclick);
+buttonStop.disabled = true;
+
+function onStartclick() {
+  if (!nIntervId) {
+    nIntervId = setInterval(changeColor, 1000);
+    buttonStop.disabled = false;
+    buttonStart.disabled = true;
+  }
+}
+
+function onStopclick() {
+  clearInterval(nIntervId);
+  nIntervId = null;
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+  body.style.backgroundColor = 'unset';
+}
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
